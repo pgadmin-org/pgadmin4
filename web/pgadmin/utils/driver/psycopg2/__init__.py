1ba5d79978db7cf02eabfa03e2b810842327c714
Ashesh Vashi <ashesh.vashi@enterprisedb.com>
2016-01-04 13:13:27 +0530

    Allow to specify the number of arguments in the qtIdent to make it work with schema specified object name. This will also works with EntepriseDB's package too.
    Also, added dependency on the current connection object, it will allow
    us to use the keywords list specific to the database server in futuer.
    
    You can use the qtIdent template filter as:
    conn|qtIdent(name)
    or,
    conn|qtIdent(schema_name, name)

--- web/pgadmin/utils/driver/psycopg2/__init__.py
+++ web/pgadmin/utils/driver/psycopg2/__init__.py
@@ -859,16 +859,22 @@ class Driver(BaseDriver):
             return result
 
     @staticmethod
-    def qtIdent(value):
+    def qtIdent(conn, *args):
+        # We're not using the conn object at the moment, but - we will modify the
+        # logic to use the server version specific keywords later.
+        res = None
+        value = None
 
-        if (len(value) == 0):
-            return value
+        for val in args:
+            if len(val) == 0:
+                continue
 
-        result = value;
+            value = val
 
-        if (Driver.needsQuoting(result, False)):
-            result.replace("\"", "\"\"")
-            return "\"" + result + "\""
-        else:
-            return result;
+            if (Driver.needsQuoting(val, False)):
+                value.replace("\"", "\"\"")
+                value = "\"" + value + "\""
 
+            res = ((res and res + '.') or '') + value
+
+        return res
