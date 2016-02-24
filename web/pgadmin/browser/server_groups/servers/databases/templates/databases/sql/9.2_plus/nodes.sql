f4ae7cd210732c5b35925988861e8a050692955f
Ashesh Vashi <ashesh.vashi@enterprisedb.com>
2016-02-24 15:00:13 +0530

    Resolved a typo in the node creation SQL template for PostgreSQL 9.2.

--- web/pgadmin/browser/server_groups/servers/databases/templates/databases/sql/9.2_plus/nodes.sql
+++ web/pgadmin/browser/server_groups/servers/databases/templates/databases/sql/9.2_plus/nodes.sql
@@ -1,6 +1,6 @@
 SELECT
     db.oid as did, db.datname as name, ta.spcname as spcname, db.datallowconn,
-    has_database_privilege(db.oid, 'CREATE') as cancreate, , datdba as owner
+    has_database_privilege(db.oid, 'CREATE') as cancreate, datdba as owner
 FROM
     pg_database db
     LEFT OUTER JOIN pg_tablespace ta ON db.dattablespace = ta.oid{% if did %}
