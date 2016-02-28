b85f9e6d1a8a5e15b9c4896917c464931c1c55a1
Ashesh Vashi <ashesh.vashi@enterprisedb.com>
2016-02-28 12:49:28 +0530

    Creating an empty pgAdmin module container (named tools) for keeping all the sub-modules of type tools.

---
+++ web/pgadmin/tools/__init__.py
@@ -0,0 +1,25 @@
+##########################################################################
+#
+# pgAdmin 4 - PostgreSQL Tools
+#
+# Copyright (C) 2013 - 2016, The pgAdmin Development Team
+# This software is released under the PostgreSQL Licence
+#
+##########################################################################
+
+"""A blueprint module container for keeping all submodule of type tool."""
+
+from pgadmin.utils import PgAdminModule
+from pgadmin.utils.ajax import bad_request
+from flask.ext.babel import gettext
+
+MODULE_NAME = 'tools'
+
+# Initialise the module
+blueprint = PgAdminModule(MODULE_NAME, __name__)
+
+
+@blueprint.route("/")
+def index():
+    """Calling tools index URL directly is not allowed."""
+    return bad_request(gettext('This URL can not be requested directly!'))
