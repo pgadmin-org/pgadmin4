954163b4957c1dce89c0632d009b845cbe90faf4
Ashesh Vashi <ashesh.vashi@enterprisedb.com>
2016-06-01 15:39:54 +0530

    Resolved an typo in Backup dialog.
    Reported by Susan Douglas.

--- web/pgadmin/tools/backup/templates/backup/js/backup.js
+++ web/pgadmin/tools/backup/templates/backup/js/backup.js
@@ -129,7 +129,7 @@ TODO LIST FOR BACKUP:
           {label: "Directory", value: "directory"}
         ]
       },{
-        id: 'ratio', label: '{{ _('Compresion ratio') }}',
+        id: 'ratio', label: '{{ _('Compression ratio') }}',
         type: 'int', min: 0, max:9, disabled: false
       },{
         id: 'encoding', label: '{{ _('Encoding') }}',
