e06fd21f3cbe7fafb308c77ef040b71ac1c47325
Dave Page <dpage@pgadmin.org>
2016-09-26 16:16:13 +0100

    Bump version for release

--- web/config.py
+++ web/config.py
@@ -36,16 +36,19 @@ APP_REVISION = 0
 
 # Application version suffix, e.g. 'beta1', 'dev'. Usually an empty string
 # for GA releases.
-APP_SUFFIX = 'rc1'
+APP_SUFFIX = ''
 
 # Numeric application version for upgrade checks. Should be in the format:
 # [X]XYYZZ, where X is the release version, Y is the revision, with a leading
 # zero if needed, and Z represents the suffix, with a leading zero if needed
-APP_VERSION_INT = 10005
+APP_VERSION_INT = 10006
 
 # DO NOT CHANGE!
 # The application version string, constructed from the components
-APP_VERSION = '%s.%s-%s' % (APP_RELEASE, APP_REVISION, APP_SUFFIX)
+if not APP_SUFFIX:
+    APP_VERSION = '%s.%s' % (APP_RELEASE, APP_REVISION)
+else:
+    APP_VERSION = '%s.%s-%s' % (APP_RELEASE, APP_REVISION, APP_SUFFIX)
 
 # Copyright string for display in the app
 APP_COPYRIGHT = 'Copyright 2013 - 2016, The pgAdmin Development Team'
