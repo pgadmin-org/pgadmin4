b19081421b980fbfe8c125201ea7f55e7d6a9c48
Dave Page <dpage@pgadmin.org>
2017-02-24 13:58:23 +0000

    Ensure primary key column names are quoted in View Data mode of the Query Tool. Fixes #2208

--- web/pgadmin/tools/sqleditor/templates/sqleditor/sql/default/objectquery.sql
+++ web/pgadmin/tools/sqleditor/templates/sqleditor/sql/default/objectquery.sql
@@ -4,7 +4,7 @@ SELECT * FROM {{ conn|qtIdent(nsp_name, object_name) }}
 WHERE {{ sql_filter }}
 {% endif %}
 {% if primary_keys %}
-ORDER BY {% for p in primary_keys %}{{p}}{% if cmd_type == 1 or cmd_type == 3 %} ASC{% elif cmd_type == 2 %} DESC{% endif %}
+ORDER BY {% for p in primary_keys %}{{conn|qtIdent(p)}}{% if cmd_type == 1 or cmd_type == 3 %} ASC{% elif cmd_type == 2 %} DESC{% endif %}
 {% if not loop.last %}, {% else %} {% endif %}{% endfor %}
 {% endif %}
 {% if limit > 0 %}
