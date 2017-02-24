c0f2aab001c24bcbbc96bd0d1df00989d0e36012
Surinder Kumar <surinder.kumar@enterprisedb.com>
2017-02-24 13:02:13 +0000

    Ensure we never sort columns on the treeview. Fixes #2185

--- web/pgadmin/browser/templates/browser/js/browser.js
+++ web/pgadmin/browser/templates/browser/js/browser.js
@@ -34,9 +34,13 @@ function(require, $, _, S, Bootstrap, pgAdmin, Alertify, CodeMirror) {
   };
 
   var processTreeData = function(payload) {
-    var data = JSON.parse(payload).data.sort(function(a, b) {
-        return pgAdmin.natural_sort(a.label, b.label, {'_type': a._type});
-    });
+    var data = JSON.parse(payload).data;
+    if (data.length && data[0]._type !== 'column' &&
+        data[0]._type !== 'catalog_object_column') {
+      data = data.sort(function(a, b) {
+          return pgAdmin.natural_sort(a.label, b.label);
+      });
+    }
     _.each(data, function(d){
       d._label = d.label;
       d.label = _.escape(d.label);
@@ -868,7 +872,7 @@ function(require, $, _, S, Bootstrap, pgAdmin, Alertify, CodeMirror) {
                           d = ctx.t.itemData(i);
                           if (
                             pgAdmin.natural_sort(
-                              d._label, _data._label, {'_type': d._type}
+                              d._label, _data._label
                             ) == 1
                           )
                             return true;
@@ -893,7 +897,7 @@ function(require, $, _, S, Bootstrap, pgAdmin, Alertify, CodeMirror) {
                           d = ctx.t.itemData(i);
                           if (
                             pgAdmin.natural_sort(
-                              d._label, _data._label, {'_type': d._type}
+                              d._label, _data._label
                             ) != -1
                           )
                             return true;
@@ -901,7 +905,7 @@ function(require, $, _, S, Bootstrap, pgAdmin, Alertify, CodeMirror) {
                           d = ctx.t.itemData(i);
                           if (
                             pgAdmin.natural_sort(
-                              d._label, _data._label, {'_type': d._type}
+                              d._label, _data._label
                             ) != 1
                           )
                             return true;
@@ -910,7 +914,7 @@ function(require, $, _, S, Bootstrap, pgAdmin, Alertify, CodeMirror) {
                           d = ctx.t.itemData(i);
                           if (
                             pgAdmin.natural_sort(
-                              d._label, _data._label, {'_type': d._type}
+                              d._label, _data._label
                             ) == 1
                           ) {
                             s = m + 1;
