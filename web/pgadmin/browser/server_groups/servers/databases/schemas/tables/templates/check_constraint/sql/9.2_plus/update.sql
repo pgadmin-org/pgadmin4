685b1ea12fd9ce7abb0bfcfb9e851d561a330760
Harshal Dhumal <harshal.dhumal@enterprisedb.com>
2017-02-24 10:44:43 +0000

    Fix renaming of check constraints when the table name is changed at the same time. Fixes #2201

--- web/pgadmin/browser/server_groups/servers/databases/schemas/tables/templates/check_constraint/sql/9.2_plus/update.sql
+++ web/pgadmin/browser/server_groups/servers/databases/schemas/tables/templates/check_constraint/sql/9.2_plus/update.sql
@@ -1,13 +1,13 @@
 {% if data %}
 {% if data.name != o_data.name %}
-ALTER TABLE {{ conn|qtIdent(o_data.nspname, o_data.relname) }}
+ALTER TABLE {{ conn|qtIdent(o_data.nspname, data.table) }}
     RENAME CONSTRAINT {{ conn|qtIdent(o_data.name) }} TO {{ conn|qtIdent(data.name) }};{% endif -%}
 {% if 'convalidated' in data and o_data.convalidated != data.convalidated and not data.convalidated %}
 
-ALTER TABLE {{ conn|qtIdent(o_data.nspname, o_data.relname) }}
+ALTER TABLE {{ conn|qtIdent(o_data.nspname, data.table) }}
     VALIDATE CONSTRAINT {{ conn|qtIdent(data.name) }};{% endif -%}
 {% if data.comment is defined and data.comment != o_data.comment %}
 
-COMMENT ON CONSTRAINT {{ conn|qtIdent(data.name) }} ON {{ conn|qtIdent(o_data.nspname, o_data.relname) }}
+COMMENT ON CONSTRAINT {{ conn|qtIdent(data.name) }} ON {{ conn|qtIdent(o_data.nspname, data.table) }}
     IS {{ data.comment|qtLiteral }};{% endif %}
 {% endif -%}
