b826e0863c517be3711a146c181b396002adde77
Dave Page <dpage@pgadmin.org>
2017-03-30 16:49:11 -0400

    Clean the build from the correct directory.

--- ci/build_runtime_qt5.sh
+++ ci/build_runtime_qt5.sh
@@ -5,7 +5,7 @@ echo "Building runtime - QT5..."
 echo "################################################################################"
 echo
 
-make clean
 cd $WORKSPACE/runtime
+make clean
 PATH=/usr/local/python-$PYTHON_VERSION/bin:$PATH /bin/qmake-qt5 DEFINES+=PGADMIN4_USE_WEBKIT
 make all
