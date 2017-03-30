8941756a9d3e3b785b8567fc731221c2ab18d1d8
Dave Page <dpage@pgadmin.org>
2017-03-30 17:11:08 -0400

    Select the correct Sphinx version

--- ci/build_pip_wheel.sh
+++ ci/build_pip_wheel.sh
@@ -6,6 +6,13 @@ echo "##########################################################################
 echo
 
 . $WORKSPACE/pgadmin-venv/bin/activate
-$WORKSPACE/pgadmin-venv/bin/pip install Sphinx==1.4.9
+
+SPHINX_VER=""
+if [ "$PYTHON_VERSION" = "2.6" -o ""$PYTHON_VERSION" = "3.3" ]; then
+    SPHINX_VER="==1.4.9"
+fi
+
+$WORKSPACE/pgadmin-venv/bin/pip install Sphinx$SPHINX_VER
+
 cd $WORKSPACE/
 make pip
