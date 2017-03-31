5ce6cd8ca051e73e90533df92eddb3415c7bf29d
Dave Page <dpage@pgadmin.org>
2017-03-30 22:47:05 -0400

    Fix a thinko. Had to be one following those typos...

--- ci/run_python_tests.sh
+++ ci/run_python_tests.sh
@@ -20,7 +20,7 @@ if [[ "$PYTHON_VERSION" == 3* ]]; then
     PYTHON_SUFFIX="3"
 fi
 
-/usr/bin/virtualenv -p /usr/local/python-$PYTHON_VERSION$PYTHON_SUFFIX/bin/python $WORKSPACE/pgadmin-venv
+/usr/bin/virtualenv -p /usr/local/python-$PYTHON_VERSION/bin/python$PYTHON_SUFFIX $WORKSPACE/pgadmin-venv
 . $WORKSPACE/pgadmin-venv/bin/activate
 $WORKSPACE/pgadmin-venv/bin/pip install -r requirements.txt
 $WORKSPACE/pgadmin-venv/bin/pip install -r web/regression/requirements.txt
