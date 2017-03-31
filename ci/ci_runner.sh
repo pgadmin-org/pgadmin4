7a1bb8863a8759263636b0b8aa77ad462f3ce4d4
Dave Page <dpage@pgadmin.org>
2017-03-31 12:26:57 -0400

    Include translation message catalogs in CI build/test runs.

--- ci/ci_runner.sh
+++ ci/ci_runner.sh
@@ -6,5 +6,6 @@ $WORKSPACE/ci/run_jasmine_tests.sh || { echo 'Error detected when running the Ja
 $WORKSPACE/ci/build_docs.sh || { echo 'Failed to build the documentation.' ; exit 1; }
 $WORKSPACE/ci/build_runtime_qt4.sh || { echo 'Failed to build the QT4 runtime.' ; exit 1; }
 $WORKSPACE/ci/build_runtime_qt5.sh || { echo 'Failed to build the QT5 runtime.' ; exit 1; }
+$WORKSPACE/ci/update_messages.sh || { echo 'Failed to update translation message catalogs.' ; exit 1; }
 $WORKSPACE/ci/build_pip_wheel.sh || { echo 'Failed to build the PIP wheel.' ; exit 1; }
 $WORKSPACE/ci/build_tarballs.sh || { echo 'Failed to build the tarballs.' ; exit 1; }
