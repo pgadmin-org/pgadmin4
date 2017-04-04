dd33332eb065b1a4f4f3bcbff1f3426e5dbcaef1
Rajasekar <rajasekarg@syncfusion.com>
2017-04-04 21:31:02 +0530

    POS-34 -Save Button issue without table name

--- web/pgadmin/browser/server_groups/servers/databases/schemas/tables/templates/table/js/table.js
+++ web/pgadmin/browser/server_groups/servers/databases/schemas/tables/templates/table/js/table.js
@@ -734,7 +734,6 @@ function($, _, S, pgAdmin, pgBrowser, alertify) {
               schema = this.get('schema'),
               relowner = this.get('relowner');
 
-          this.errorModel.clear();
 
           // If nothing to validate or VacuumSetting keys then
           // return from here
@@ -743,6 +742,9 @@ function($, _, S, pgAdmin, pgBrowser, alertify) {
                         || _.indexOf(keys, 'toast_autovacuum_enabled') != -1) ) {
             return null;
           }
+
+          // Have to clear  existing validation before initate current state validation only
+          this.errorModel.clear();
 
           if (_.isUndefined(name) || _.isNull(name) ||
             String(name).replace(/^\s+|\s+$/g, '') == '') {
