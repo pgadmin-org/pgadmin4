f6ffaaff98f117e69c34c7bc30dd7f61b971c9ae
Dave Page <dpage@pgadmin.org>
2017-04-21 10:15:05 +0100

    Revert "Ensure PIP is the latest version when running the CI tests."
    This reverts commit bf1f4aefa686554fa24ac2fbcf1457aa9de4eccf.

--- ci/run_python_tests.sh
+++ ci/run_python_tests.sh
@@ -23,7 +23,6 @@ fi
 
 /usr/bin/virtualenv -p /usr/local/python-$PYTHON_VERSION/bin/python$PYTHON_SUFFIX $WORKSPACE/pgadmin-venv || { echo 'ERROR: Failed to create the Python virtual environment.' ; exit 1; }
 . $WORKSPACE/pgadmin-venv/bin/activate || { echo 'ERROR: Failed to activate the Python virtual environment.' ; exit 1; }
-$WORKSPACE/pgadmin-venv/bin/pip install pip --upgrade || { echo 'ERROR: Failed to update PIP to the latest version.' ; exit 1; }
 $WORKSPACE/pgadmin-venv/bin/pip install -r requirements.txt || { echo 'ERROR: Failed to install the application requirements.' ; exit 1; }
 $WORKSPACE/pgadmin-venv/bin/pip install -r web/regression/requirements.txt || { echo 'ERROR: Failed to install the regression test requirements.' ; exit 1; }
 
