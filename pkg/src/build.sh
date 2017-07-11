6ad4a623c47154a2e3938805af52776039c23601
Dave Page <dpage@pgadmin.org>
2017-07-11 09:12:09 +0100

    Ensure dependencies are packaged in the tarball

--- pkg/src/build.sh
+++ pkg/src/build.sh
@@ -69,20 +69,19 @@ do
     tar cf - $FILE | (cd src-build/$TARBALL_NAME; tar xf -)
 done
 
-pushd web
-    yarn install
-    yarn run bundle
+cd src-build/$TARBALL_NAME/web
+yarn install
+yarn run bundle
 
-    for FILE in `ls -d pgAdmin/static/js/generated/*`
-    do
-        echo Adding $FILE
-        tar cf - $FILE | (cd ../src-build/$TARBALL_NAME/web; tar xf -)
-    done
-popd
+for FILE in `ls -d pgAdmin/static/js/generated/*`
+do
+    echo Adding $FILE
+    tar cf - $FILE | (cd ../src-build/$TARBALL_NAME/web; tar xf -)
+done
 
 # Create the tarball
 echo Creating tarball...
-cd src-build
+cd ../..
 tar zcvf ../dist/$TARBALL_NAME.tar.gz $TARBALL_NAME
 cd ..
 
