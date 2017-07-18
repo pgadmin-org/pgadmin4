659aeeb58e6a91f67b546e1d3fa06735cf453c68
Surinder Kumar <surinder.kumar@enterprisedb.com>
2017-07-18 16:30:18 +0100

    Add a missing dependency.

--- web/pgadmin/static/bundle/slickgrid.js
+++ web/pgadmin/static/bundle/slickgrid.js
@@ -1,4 +1,5 @@
 import 'slickgrid/lib/jquery.event.drag-2.3.0';
+import 'slickgrid/lib/jquery-ui-1.11.3';
 import 'slickgrid/slick.core';
 import 'slickgrid/slick.grid';
 import 'slickgrid/slick.dataview';
