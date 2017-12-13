6898b0e4c5c2dfd29a5d1ece77cb46312fbb5870
Murtuza Zabuawala <murtuza.zabuawala@enterprisedb.com>
2017-12-13 15:58:00 +0000

    Don't un-comment code with alt+. in the query tool. It's only supposed to respond to ctrl/cmd+. Fixes #2769

--- web/pgadmin/static/js/sqleditor/keyboard_shortcuts.js
+++ web/pgadmin/static/js/sqleditor/keyboard_shortcuts.js
@@ -23,16 +23,22 @@ function keyboardShortcuts(sqlEditorController, queryToolActions, event) {
   } else if (keyCode === F8_KEY) {
     event.preventDefault();
     queryToolActions.download(sqlEditorController);
-  } else if (((this.isMac() && event.metaKey) || (!this.isMac() && event.ctrlKey)) &&
-    event.shiftKey && keyCode === FWD_SLASH_KEY) {
+  } else if ((
+     (this.isMac() && event.metaKey) ||
+     (!this.isMac() && event.ctrlKey)
+    ) && !event.altKey && event.shiftKey && keyCode === FWD_SLASH_KEY) {
     _stopEventPropagation();
     queryToolActions.commentBlockCode(sqlEditorController);
-  } else if (((this.isMac() && event.metaKey) || (!this.isMac() && event.ctrlKey)) &&
-    keyCode === FWD_SLASH_KEY) {
+  } else if ((
+     (this.isMac() && !this.isKeyCtrlAltShift(event) && event.metaKey) ||
+     (!this.isMac() && !this.isKeyAltShift(event) && event.ctrlKey)
+    ) && keyCode === FWD_SLASH_KEY) {
     _stopEventPropagation();
     queryToolActions.commentLineCode(sqlEditorController);
-  } else if (((this.isMac() && event.metaKey) || (!this.isMac() && event.ctrlKey)) &&
-    keyCode === PERIOD_KEY) {
+  } else if ((
+     (this.isMac() && !this.isKeyCtrlAltShift(event) && event.metaKey) ||
+     (!this.isMac() && !this.isKeyAltShift(event) && event.ctrlKey)
+    ) && keyCode === PERIOD_KEY) {
     _stopEventPropagation();
     queryToolActions.uncommentLineCode(sqlEditorController);
   }
@@ -49,7 +55,27 @@ function isMac() {
   return window.navigator.platform.search('Mac') != -1;
 }
 
+function isKeyCtrlAlt(event) {
+  return event.ctrlKey || event.altKey;
+}
+
+function isKeyAltShift(event) {
+  return event.altKey || event.shiftKey;
+}
+
+function isKeyCtrlShift(event) {
+  return event.ctrlKey || event.shiftKey;
+}
+
+function isKeyCtrlAltShift(event) {
+  return event.ctrlKey || event.altKey || event.shiftKey;
+}
+
 module.exports = {
   processEvent: keyboardShortcuts,
   isMac: isMac,
+  isKeyCtrlAlt: isKeyCtrlAlt,
+  isKeyAltShift: isKeyAltShift,
+  isKeyCtrlShift: isKeyCtrlShift,
+  isKeyCtrlAltShift: isKeyCtrlAltShift
 };
