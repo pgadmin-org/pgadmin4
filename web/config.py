a23d0a73ade37c74a12610d0333f386133b15c9b
Dave Page <dpage@pgadmin.org>
2017-12-18 10:35:58 +0000

    Bump version for some dev builds.

--- web/config.py
+++ web/config.py
@@ -51,16 +51,16 @@ APP_ICON = 'pg-icon'
 
 # Application version number components
 APP_RELEASE = 2
-APP_REVISION = 0
+APP_REVISION = 1
 
 # Application version suffix, e.g. 'beta1', 'dev'. Usually an empty string
 # for GA releases.
-APP_SUFFIX = ''
+APP_SUFFIX = 'dev'
 
 # Numeric application version for upgrade checks. Should be in the format:
 # [X]XYYZZ, where X is the release version, Y is the revision, with a leading
 # zero if needed, and Z represents the suffix, with a leading zero if needed
-APP_VERSION_INT = 20003
+APP_VERSION_INT = 20004
 
 # DO NOT CHANGE!
 # The application version string, constructed from the components
