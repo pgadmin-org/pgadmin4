8148a61279d453eb78209105275295364430a241
Dave Page <dpage@pgadmin.org>
2018-02-20 10:44:01 +0000

    Attempt to ensure the runtime is built with the correct Python version.

--- ci/build_runtime_qt4.sh
+++ ci/build_runtime_qt4.sh
@@ -3,8 +3,14 @@
 echo "EXECUTING: Build runtime - QT4"
 echo
 
+if [ ${PYTHON_VERSION:0:1} == "3" ]; then
+    CONFIG=/usr/local/python-$PYTHON_VERSION/bin/python-config3
+else
+    CONFIG=/usr/local/python-$PYTHON_VERSION/bin/python-config
+fi
+
 cd $WORKSPACE/runtime
 
 make clean
-PATH=/usr/local/python-$PYTHON_VERSION/bin:$PATH /bin/qmake-qt4 || { echo 'ERROR: Failed to run the QT4 qmake step.' ; exit 1; }
+PATH=/usr/local/python-$PYTHON_VERSION/bin:$PATH PYTHON_CONFIG=$CONFIG /bin/qmake-qt4 || { echo 'ERROR: Failed to run the QT4 qmake step.' ; exit 1; }
 make all || { echo 'ERROR: Failed to build the QT4 runtime.' ; exit 1; }
