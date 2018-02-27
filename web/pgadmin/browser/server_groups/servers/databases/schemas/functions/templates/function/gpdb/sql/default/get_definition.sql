3be22383b81b05b8f5fb62173809e85faaac638b
Joao Pedro De Almeida Pereira <jdealmeidapereira@pivotal.io>
2018-02-27 11:05:15 +0000

    Fix function reserve SQL for GPDB. Fixes #3150

--- web/pgadmin/browser/server_groups/servers/databases/schemas/functions/templates/function/gpdb/sql/default/get_definition.sql
+++ web/pgadmin/browser/server_groups/servers/databases/schemas/functions/templates/function/gpdb/sql/default/get_definition.sql
@@ -6,8 +6,10 @@ SELECT proretset, prosrc, probin,
   proconfig, procost, prorows, prodataaccess,
   'a' as proexeclocation,
   (SELECT lanname FROM pg_catalog.pg_language WHERE pg_proc.oid = prolang) as lanname,
-  nspname || '.' || pg_proc.proname || '(' || COALESCE(pg_catalog.pg_get_function_identity_arguments(pg_proc.oid), '') || ')' as name,
-  nspname || '.' || pg_proc.proname || '(' || COALESCE(pg_catalog.pg_get_function_arguments(pg_proc.oid), '') || ')' as name_with_default_args
+  COALESCE(pg_catalog.pg_get_function_identity_arguments(pg_proc.oid), '') AS func_with_identity_arguments,
+  nspname,
+  proname,
+  COALESCE(pg_catalog.pg_get_function_arguments(pg_proc.oid), '') AS func_args
 FROM pg_catalog.pg_proc
   JOIN pg_namespace nsp ON nsp.oid=pg_proc.pronamespace
 WHERE proisagg = FALSE
