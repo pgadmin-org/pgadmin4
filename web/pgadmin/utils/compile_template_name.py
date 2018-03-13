9365ab62fee14c570d0e37fa4f0cac3b79a66e72
Dave Page <dpage@pgadmin.org>
2018-03-13 08:49:34 -0400

    Revert "Fix template path generation for Windows. Fixes #3139"
    This reverts commit 695d4e648cd4cd3a086b407e471932537a793d4b.
    
    It's borked!

--- web/pgadmin/utils/compile_template_name.py
+++ web/pgadmin/utils/compile_template_name.py
@@ -11,13 +11,10 @@ import os
 
 def compile_template_name(
         template_prefix, template_file_name, server_type, version):
-
-    # Template path concatenation should be same as
-    # Ref: ../pgadmin4/web/pgadmin/utils/versioned_template_loader.py +54
-    # to avoid path mismatch in windows
-    return compile_template_path(template_prefix, server_type, version) + \
-           '/' + template_file_name
-
+    return os.path.join(
+        compile_template_path(template_prefix, server_type, version),
+        template_file_name
+    )
 
 
 def compile_template_path(template_prefix, server_type, version):
@@ -25,8 +22,4 @@ def compile_template_path(template_prefix, server_type, version):
         version_path = '#{0}#{1}#'.format(server_type, version)
     else:
         version_path = '#{0}#'.format(version)
-
-    # Template path concatenation should be same as
-    # Ref: ../pgadmin4/web/pgadmin/utils/versioned_template_loader.py +54
-    # to avoid path mismatch in windows
-    return template_prefix + '/' + version_path
+    return os.path.join(template_prefix, version_path)
