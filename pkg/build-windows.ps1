b6fda3238d27e50200238336f9e35f1b4a7dd9f0
Dave Page <dpage@pgadmin.org>
2018-07-06 16:46:00 +0100

    Un-corrupt Windows build script

--- pkg/build-windows.ps1
+++ pkg/build-windows.ps1
@@ -1,4 +1,4 @@
-﻿﻿function New-TemporaryDirectory {
+﻿function New-TemporaryDirectory {
     $parent = [System.IO.Path]::GetTempPath()
     $name = [System.IO.Path]::GetRandomFileName()
     New-Item -ItemType Directory -Path (Join-Path $parent $name)
