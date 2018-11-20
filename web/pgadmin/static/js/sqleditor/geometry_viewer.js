a0878563c2e35122a756fb0f923ae9da76fa0197
Shivam Rai <34955127+ShivamRai2003@users.noreply.github.com>
2018-11-21 00:10:06 +0530

    GCI : Task > PostGIS: Fix some strings in pgAdmin4 geometry viewer - OSGEO

--- web/pgadmin/static/js/sqleditor/geometry_viewer.js
+++ web/pgadmin/static/js/sqleditor/geometry_viewer.js
@@ -25,7 +25,7 @@ let GeometryViewer = {
     if (self.panel_closed) {
       let wcDocker = window.wcDocker;
       let geometry_viewer_panel = handler.gridView.geometry_viewer =
-        handler.gridView.docker.addPanel('geometry_viewer',
+        handler.gridView.docker.addPanel('geometry_viewer'),
           wcDocker.DOCK.STACKED, handler.gridView.data_output_panel);
       $('#geometry_viewer_panel')[0].appendChild(self.map_component.mapContainer.get(0));
       self.panel_closed = false;
@@ -201,7 +201,7 @@ function initMapComponent() {
         vectorLayer.addData(geoJSONs);
       } catch (e) {
         // Invalid LatLng object: (NaN, NaN)
-        infoList.push('An error occurred while rendering data.');
+        infoList.push(gettext('An error occurred while rendering data.'));
         isEmpty = true;
       }
 
@@ -284,7 +284,7 @@ function parseData(items, columns, columnIndex) {
     tooManyGeometries = false;
 
   if (items.length === 0) {
-    infoList.push('Empty row.');
+    infoList.push(gettext('Empty row.'));
     return {
       'geoJSONs': [],
       'selectedSRID': 0,
