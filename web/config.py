252429df50a27a7a6499358441f703463dbac796
Akshay Joshi <akshay.joshi@enterprisedb.com>
2019-06-28 09:40:31 -0400

    Update version for release

--- web/config.py
+++ web/config.py
@@ -50,7 +50,7 @@ APP_ICON = 'pg-icon'
 
 # Application version number components
 APP_RELEASE = 4
-APP_REVISION = 9
+APP_REVISION = 10
 
 # Application version suffix, e.g. 'beta1', 'dev'. Usually an empty string
 # for GA releases.
@@ -59,7 +59,7 @@ APP_SUFFIX = ''
 # Numeric application version for upgrade checks. Should be in the format:
 # [X]XYYZZ, where X is the release version, Y is the revision, with a leading
 # zero if needed, and Z represents the suffix, with a leading zero if needed
-APP_VERSION_INT = 40900
+APP_VERSION_INT = 41000
 
 # DO NOT CHANGE!
 # The application version string, constructed from the components
