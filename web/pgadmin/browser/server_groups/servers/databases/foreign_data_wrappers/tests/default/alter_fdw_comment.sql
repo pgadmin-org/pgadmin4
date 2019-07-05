ba32c98241b77c27215096db121afe0984d8ea73
Aditya Toshniwal <aditya.toshniwal@enterprisedb.com>
2019-07-05 11:24:47 +0100

    Add Reverse Engineered SQL tests for FDWs. Fixes #4441

---
+++ web/pgadmin/browser/server_groups/servers/databases/foreign_data_wrappers/tests/default/alter_fdw_comment.sql
@@ -0,0 +1,11 @@
+-- Foreign Data Wrapper: Fdw1_$%{}[]()&*^!@"'`\/#
+
+-- DROP FOREIGN DATA WRAPPER Fdw1_$%{}[]()&*^!@"'`\/#
+
+CREATE FOREIGN DATA WRAPPER "Fdw1_$%{}[]()&*^!@""'`\/#";
+
+ALTER FOREIGN DATA WRAPPER "Fdw1_$%{}[]()&*^!@""'`\/#"
+    OWNER TO postgres;
+
+COMMENT ON FOREIGN DATA WRAPPER "Fdw1_$%{}[]()&*^!@""'`\/#"
+    IS 'a comment';
