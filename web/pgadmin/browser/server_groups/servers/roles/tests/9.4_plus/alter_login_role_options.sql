1ddc9cc889b52d6d2ee264f8a1515bc014c2232f
Murtuza Zabuawala <murtuza.zabuawala@enterprisedb.com>
2019-08-26 18:25:48 +0530

    Modified RE-SQL test to cover Password, Account Expire, Connection Limit and Variables for Roles.

--- web/pgadmin/browser/server_groups/servers/roles/tests/9.4_plus/alter_login_role_options.sql
+++ web/pgadmin/browser/server_groups/servers/roles/tests/9.4_plus/alter_login_role_options.sql
@@ -7,6 +7,10 @@ CREATE ROLE "Role2_$%{}[]()&*^!@""'`\/#" WITH
   INHERIT
   NOCREATEDB
   CREATEROLE
-  REPLICATION;
+  NOREPLICATION
+  CONNECTION LIMIT 100
+  VALID UNTIL '2050-01-01 00:00:00+05:30';
+
+ALTER ROLE "Role2_$%{}[]()&*^!@""'`\/#" IN DATABASE postgres SET application_name TO 'pg4';
 
 COMMENT ON ROLE "Role2_$%{}[]()&*^!@""'`\/#" IS 'This is detailed description';
