7ed6328864df1c19c1ad1d40825456f2b088b8c5
Eldhose M Joy <eldhosemjoy@yahoo.com>
2020-04-21 00:03:12 +0530

    Update pgAdmin4.py
    This helps to reverse proxy the app on the servers without hassle on nginx or any reverse proxies.

--- web/pgAdmin4.py
+++ web/pgAdmin4.py
@@ -95,7 +95,27 @@ class ReverseProxied(object):
             environ["wsgi.url_scheme"] = scheme
         return self.app(environ, start_response)
 
+##########################################################################
+# Support Prefix Base Path
+##########################################################################
 
+class PrefixMiddleware(object):
+
+    def __init__(self, app, prefix=''):
+        self.app = app
+        self.prefix = prefix
+
+    def __call__(self, environ, start_response):
+
+        if environ['PATH_INFO'].startswith(self.prefix):
+            environ['PATH_INFO'] = environ['PATH_INFO'][len(self.prefix):]
+            environ['SCRIPT_NAME'] = self.prefix
+            return self.app(environ, start_response)
+        else:
+            start_response('404', [('Content-Type', 'text/plain')])
+            return ["This url does not belong to the app.".encode()]
+
+    
 ##########################################################################
 # Server startup
 ##########################################################################
@@ -109,6 +129,8 @@ if config.DEBUG:
 app = create_app()
 
 if config.SERVER_MODE:
+    if(config.BASE_SERVER_PATH!="/"):
+        app.wsgi_app = PrefixMiddleware(app.wsgi_app, prefix=config.BASE_SERVER_PATH)
     app.wsgi_app = ReverseProxied(app.wsgi_app)
 
 if config.DEBUG:
