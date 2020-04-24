b7dad25d3eb29fae05fef4f161dea644f080276f
Dave Page <dpage@pgadmin.org>
2020-04-24 16:20:25 +0100

    We support Python 3.8

--- pkg/pip/setup_pip.py
+++ pkg/pip/setup_pip.py
@@ -90,7 +90,8 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7'
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
     ],
 
     keywords='pgadmin4,postgresql,postgres',
