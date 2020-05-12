8a06a836f41d4e7dc932fe4bbbdb5e746c27bd7e
Akshay Joshi <akshay.joshi@enterprisedb.com>
2020-05-12 17:22:04 +0530

    Ensure that pgAdmin4 should be compiled with older OSX SDK's.

--- runtime/macos.mm
+++ runtime/macos.mm
@@ -10,13 +10,18 @@
 //////////////////////////////////////////////////////////////////////////
 
 #import <Cocoa/Cocoa.h>
+#import <Availability.h>
 
 // Detect if we're running in Dark mode
 bool IsDarkMode() {
+#ifdef __MAC_10_14
     if (@available(macOS 10.14, *)) {
         NSString *interfaceStyle = [NSUserDefaults.standardUserDefaults valueForKey:@"AppleInterfaceStyle"];
         return [interfaceStyle isEqualToString:@"Dark"];
     } else {
         return NO;
     }
+#else
+    return NO;
+#endif
 }
