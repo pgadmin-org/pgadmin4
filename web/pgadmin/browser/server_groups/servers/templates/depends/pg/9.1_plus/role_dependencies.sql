f7214b7cfe18aeaba45fc3a70492b660de7faa30
Khushboo Vashi <khushboo.vashi@enterprisedb.com>
2021-02-02 11:49:51 +0530

    Fixed dependency tab loading issue when database name having special characters. 
    refs #6087

--- web/pgadmin/browser/server_groups/servers/templates/depends/pg/9.1_plus/role_dependencies.sql
+++ web/pgadmin/browser/server_groups/servers/templates/depends/pg/9.1_plus/role_dependencies.sql
@@ -3,6 +3,6 @@ FROM pg_shdepend dep
 LEFT JOIN pg_roles r ON refclassid=1260 AND refobjid=r.oid
 {{where_clause}}
 {% if db_name %}
- AND dep.dbid = (SELECT oid FROM pg_database WHERE datname = '{{db_name}}')
+ AND dep.dbid = (SELECT oid FROM pg_database WHERE datname = {{ db_name|qtLiteral }})
 {% endif %}
 ORDER BY 1
