2121adb7fbe9b0453230e1fc91e2a06044d9705b
Dave Page <dpage@pgadmin.org>
2021-12-06 17:06:26 +0000

    PEP-8. Sigh.

--- pkg/pip/setup_pip.py
+++ pkg/pip/setup_pip.py
@@ -56,11 +56,12 @@ setup(
     version=config.APP_VERSION,
 
     description='PostgreSQL Tools',
-    long_description='pgAdmin is the most popular and feature rich Open Source '
-                     'administration and development platform for PostgreSQL, '
-                     'the most advanced Open Source database in the world.\n\n'
-                     'pgAdmin may be used on Linux, Unix, macOS and Windows to '
-                     'manage PostgreSQL and EDB Advanced Server 10 and above.',
+    long_description='pgAdmin is the most popular and feature rich Open '
+                     'Source administration and development platform for '
+                     'PostgreSQL, the most advanced Open Source database in '
+                     'the world.\n\npgAdmin may be used on Linux, Unix, '
+                     'macOS and Windows to manage PostgreSQL and EDB '
+                     'Advanced Server 10 and above.',
 
     url='https://www.pgadmin.org/',
 
