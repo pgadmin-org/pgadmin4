2786f7ac5ad344eb23ec070e2dc29d13a35c15e0
Akshay Joshi <akshay.joshi@enterprisedb.com>
2022-01-04 13:54:25 +0530

    Update copyright notices for 2022

--- web/pgadmin/browser/server_groups/servers/databases/schemas/tables/static/js/table.js
+++ web/pgadmin/browser/server_groups/servers/databases/schemas/tables/static/js/table.js
@@ -2,7 +2,7 @@
 //234567890
 // pgAdmin 4 - PostgreSQL Tools
 //
-// Copyright (C) 2013 - 2021, The pgAdmin Development Team
+// Copyright (C) 2013 - 2022, The pgAdmin Development Team
 // This software is released under the PostgreSQL Licence
 //
 //////////////////////////////////////////////////////////////
