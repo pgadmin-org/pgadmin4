3e0d8d192039106b5674025bc75e57d29bf07c81
Dave Page <dpage@pgadmin.org>
2022-06-27 10:53:11 +0100

    Fixes for el9

--- pkg/redhat/setup.sh
+++ pkg/redhat/setup.sh
@@ -5,18 +5,21 @@ if [ "$EUID" -ne 0 ]
   exit 1
 fi
 
-OS_VERSION=$(cat /etc/os-release | grep "^VERSION_ID=" | awk -F "=" '{ print $2 }' | sed 's/"//g')
+OS_VERSION=$(cat /etc/os-release | grep "^VERSION_ID=" | awk -F "=" '{ print $2 }' | sed 's/"//g' | awk -F "." '{ print $1 }')
 
 # EPEL & other repos
-yum -y install centos-release-scl
+if [ ${OS_VERSION} != 9 ]; then
+    yum -y install centos-release-scl
+fi
+
 yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-${OS_VERSION}.noarch.rpm
-if [ ${OS_VERSION} == 8 ]; then
+if [ ${OS_VERSION} == 8 ] || [ ${OS_VERSION} == 9 ]; then
     yum config-manager --enable PowerTools AppStream BaseOS *epel
     dnf -qy module disable postgresql
 fi
 
 # PostgreSQL repo
-if [ ${OS_VERSION} == 7 ] || [ ${OS_VERSION} == 8 ]; then
+if [ ${OS_VERSION} == 7 ] || [ ${OS_VERSION} == 8 ] || [ ${OS_VERSION} == 9 ]; then
     yum install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-${OS_VERSION}-x86_64/pgdg-redhat-repo-latest.noarch.rpm
 else
     yum install -y https://download.postgresql.org/pub/repos/yum/reporpms/F-${OS_VERSION}-x86_64/pgdg-fedora-repo-latest.noarch.rpm
@@ -24,7 +27,7 @@ fi
 
 # Node repo
 echo "Setting up the NodeJS repo..."
-curl -sL https://rpm.nodesource.com/setup_12.x | bash -
+curl -sL https://rpm.nodesource.com/setup_16.x | bash -
 
 # Yarn repo
 echo "Setting up the Yarn repo..."
@@ -36,6 +39,9 @@ yum groupinstall -y "Development Tools"
 
 if [ ${OS_VERSION} == 7 ]; then
     yum install -y expect fakeroot httpd-devel postgresql14-devel python3-devel nodejs yarn rpm-build rpm-sign yum-utils krb5-devel
+    pip3 install sphinx
+elif [ ${OS_VERSION} == 9 ]; then
+    yum install -y expect postgresql14-devel python3-devel nodejs yarn rpm-build rpm-sign yum-utils krb5-devel
     pip3 install sphinx
 else
     yum install -y expect fakeroot postgresql14-devel python3-devel python3-sphinx nodejs yarn rpm-build rpm-sign yum-utils krb5-devel
