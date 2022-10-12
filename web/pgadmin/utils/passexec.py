02582f1ff5e37d6607414fb4e07a5dbabfc6c7f3
Elias <elias@acuminor.com>
2022-10-12 17:49:55 +0200

    not implemented in server mode

--- web/pgadmin/utils/passexec.py
+++ web/pgadmin/utils/passexec.py
@@ -1,10 +1,13 @@
 
 
+import logging
 import subprocess
 from datetime import datetime, timedelta
 from threading import Lock
 
 from flask import current_app
+
+import config
 
 
 class PasswordExec:
@@ -19,20 +22,29 @@ class PasswordExec:
         self.last_result = None
 
     def get(self):
+        if config.SERVER_MODE:
+            # Arbitrary shell execution on server is a security risk
+            raise NotImplementedError('Passexec not available in server mode')
         with self.lock:
             if not self.password or self.is_expired():
                 if not self.cmd:
                     return None
                 current_app.logger.info(f'Calling passexec')
                 now = datetime.utcnow()
-                p = subprocess.run(
-                    self.cmd,
-                    shell = True,
-                    timeout = self.timeout,
-                    capture_output = True,
-                    text = True,
-                    check = True,
-                )
+                try:
+                    p = subprocess.run(
+                        self.cmd,
+                        shell = True,
+                        timeout = self.timeout,
+                        capture_output = True,
+                        text = True,
+                        check = True,
+                    )
+                except subprocess.CalledProcessError as e:
+                    if (e.stderr):
+                        self.create_logger().error(e.stderr)
+                    raise
+
                 current_app.logger.info(f'Passexec completed successfully')
                 self.last_result = now
                 self.password = p.stdout.strip()
@@ -42,5 +54,11 @@ class PasswordExec:
         if self.expiration_seconds == None:
             return False
         return self.last_result is not None and datetime.utcnow() - self.last_result >= timedelta(seconds=self.expiration_seconds)
+
+    def create_logger(self):
+        logger = logging.getLogger('passexec')
+        for h in current_app.logger.handlers:
+            logger.addHandler(h)
+        return logger
 
 
