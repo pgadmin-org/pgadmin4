d053742ca4b59b5dd75786ad3a0980fa5cb18c53
Khushboo Vashi <khushboo.vashi@enterprisedb.com>
2023-03-14 15:59:49 +0530

    Change psycopg-binary in setup_pip.py file.

--- pkg/pip/setup_pip.py
+++ pkg/pip/setup_pip.py
@@ -39,8 +39,8 @@ kerberos_extras = []
 # Ensure the Wheel will use psycopg-binary, not the source distro, and stick
 # gssapi in it's own list
 for index, req in enumerate(all_requires):
-    if 'psycopg' in req:
-        req = req.replace('psycopg', 'psycopg-binary')
+    if 'psycopg[c]' in req:
+        req = req.replace('psycopg[c]', 'psycopg-binary')
 
     if 'gssapi' in req:
         kerberos_extras.append(req)
