7bb8f1fd7b2ff4a6017500766d545188383e94d3
Dave Page <dpage@pgadmin.org>
2023-05-11 12:15:42 +0100

    Re-think this check. Our own driver effectively monkey-patches the psycopg encoding mappings.

--- web/regression/python_test_utils/test_utils.py
+++ web/regression/python_test_utils/test_utils.py
@@ -1187,8 +1187,8 @@ def get_server_type(server):
         version_string = version_string[0]
 
     # Handle SQL_ASCII (https://github.com/psycopg/psycopg/issues/561)
-    if connection.info.encoding == 'ascii':
-        version_string = version_string.decode()
+    version_string = version_string.decode() if \
+        hasattr(version_string, 'decode') else version_string
 
     if "EnterpriseDB" in version_string:
         return 'ppas'
