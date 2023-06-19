af916e3cfc5d57ac42666660bbc6c24d0aab0e38
Anil Sahoo <anil.sahoo@enterprisedb.com>
2023-06-19 10:35:52 +0530

    Added security_invoker option while creating a view. #6371

--- web/pgadmin/browser/server_groups/servers/databases/publications/tests/15_plus/create_publication_few_tables_schemas.sql
+++ web/pgadmin/browser/server_groups/servers/databases/publications/tests/15_plus/create_publication_few_tables_schemas.sql
@@ -3,5 +3,5 @@
 -- DROP PUBLICATION IF EXISTS test_publication_create;
 
 CREATE PUBLICATION test_publication_create
-   FOR TABLE public.test_table_publication, TABLES IN SCHEMA test_schema_publication
-   WITH (publish = 'insert, update, delete, truncate', publish_via_partition_root = false);
+    FOR TABLE public.test_table_publication, TABLES IN SCHEMA test_schema_publication
+    WITH (publish = 'insert, update, delete, truncate', publish_via_partition_root = false);
