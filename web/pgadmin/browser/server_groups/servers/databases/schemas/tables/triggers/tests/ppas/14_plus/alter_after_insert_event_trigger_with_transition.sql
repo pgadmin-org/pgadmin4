225b56e83eaaff20b230e3d2fddb7145b61b1703
pravesh-sharma <pravesh.sharma@enterprisedb.com>
2023-06-20 20:30:09 +0530

    1. Added OR REPLACE clause while creating trigger. #6370 2. Changes in resql test cases
    clean up test folder

---
+++ web/pgadmin/browser/server_groups/servers/databases/schemas/tables/triggers/tests/ppas/14_plus/alter_after_insert_event_trigger_with_transition.sql
@@ -0,0 +1,16 @@
+-- Trigger: trig_after_insert_events_with_transition_$%{}[]()&*^!@"'`\/#
+
+-- DROP TRIGGER IF EXISTS "trig_after_insert_events_with_transition_$%{}[]()&*^!@""'`\/#" ON public.tablefortrigger;
+
+CREATE OR REPLACE TRIGGER "trig_after_insert_events_with_transition_$%{}[]()&*^!@""'`\/#"
+    AFTER INSERT
+    ON public.tablefortrigger
+    REFERENCING NEW TABLE AS inserted
+    FOR EACH ROW
+    EXECUTE FUNCTION public."Trig1_$%{}[]()&*^!@""'`\/#"();
+
+COMMENT ON TRIGGER "trig_after_insert_events_with_transition_$%{}[]()&*^!@""'`\/#" ON public.tablefortrigger
+    IS 'test comment';
+
+ALTER TABLE public.tablefortrigger
+    DISABLE TRIGGER "trig_after_insert_events_with_transition_$%{}[]()&*^!@""'`\/#";
