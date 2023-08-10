e06d39b70c86eca5e4130416356cd491c14a2bc0
Dave Page <dpage@pgadmin.org>
2023-08-10 13:35:04 +0100

    Cleanup some more unnecessary artefacts from the build.

--- pkg/mac/build-functions.sh
+++ pkg/mac/build-functions.sh
@@ -294,9 +294,10 @@ _complete_bundle() {
     cp -r "${SOURCE_DIR}/web" "${BUNDLE_DIR}/Contents/Resources/"
     cd "${BUNDLE_DIR}/Contents/Resources/web" || exit
     rm -f pgadmin4.db config_local.*
-    rm -rf karma.conf.js package.json node_modules/ regression/ tools/ pgadmin/static/js/generated/.cache
+    rm -rf karma.conf.js package.json .yarn* yarn* .editorconfig .eslint* node_modules/ regression/ tools/ pgadmin/static/js/generated/.cache
     find . -name "tests" -type d -print0 | xargs -0 rm -rf
     find . -name "feature_tests" -type d -print0 | xargs -0 rm -rf
+    find . -name "__pycache__" -type d -print0 | xargs -0 rm -rf
     find . -name ".DS_Store" -print0 | xargs -0 rm -f
 
     {
