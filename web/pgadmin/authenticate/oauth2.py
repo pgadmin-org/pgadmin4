b43ea46390f159476197c1601c8e5b8cb8a2f3ca
Florian Sabonchi <sabonchi@posteo.de>
2024-02-23 22:07:57 +0100

    fix: invalidating oauth2 session

--- web/pgadmin/authenticate/oauth2.py
+++ web/pgadmin/authenticate/oauth2.py
@@ -69,11 +69,19 @@ def init_app(app):
                      methods=['GET', 'POST'])
     @pgCSRFProtect.exempt
     def oauth_logout():
+        logout_url = session['oauth2_logout_url']
+        id_token = session['oauth2_token']['id_token']
+
         if not current_user.is_authenticated:
             return redirect(get_safe_post_logout_redirect())
         for key in list(session.keys()):
             session.pop(key)
+
         logout_user()
+        if logout_url:
+            return redirect(logout_url.format(
+                redirect_uri=request.url_root,
+                id_token=id_token))
         return redirect(get_safe_post_logout_redirect())
 
     app.register_blueprint(blueprint)
@@ -201,6 +209,8 @@ class OAuth2Authentication(BaseAuthentication):
             self.oauth2_current_client].authorize_access_token()
 
         session['pass_enc_key'] = session['oauth2_token']['access_token']
+        session['oauth2_logout_url'] = self.oauth2_config[
+            self.oauth2_current_client]['OAUTH2_LOGOUT_URL']
 
         resp = self.oauth2_clients[self.oauth2_current_client].get(
             self.oauth2_config[
