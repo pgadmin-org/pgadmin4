f66bd4bcfbd4b24739ecc4be9f2d6a2e689d3651
Khushboo Vashi <khushboo.vashi@enterprisedb.com>
2024-06-05 14:11:59 +0530

    Remove the use of is_normalized function for Python 3.7.

--- web/pgadmin/tools/user_management/__init__.py
+++ web/pgadmin/tools/user_management/__init__.py
@@ -10,7 +10,11 @@
 """Implements pgAdmin4 User Management Utility"""
 
 import json
-from unicodedata import normalize, is_normalized
+
+import sys
+if sys.version_info >= (3, 8):
+    from unicodedata import normalize, is_normalized
+
 from flask import render_template, request, \
     Response, abort, current_app, session
 from flask_babel import gettext as _
@@ -441,6 +445,10 @@ def normalise_password(password):
         'SECURITY_PASSWORD_NORMALIZE_FORM',
         'NFKD'
     )
+
+    # Remove check of Python version once Python 3.7 support ends
+    if sys.version_info < (3, 8):
+        return password
 
     return password if is_normalized(normalise_form, password) else\
         normalize(normalise_form, password)
