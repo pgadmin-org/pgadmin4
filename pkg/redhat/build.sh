95ce9e976ec3f73f1f8c59b968f0880c59848be4
Khushboo Vashi <khushboo.vashi@enterprisedb.com>
2024-06-24 18:20:19 +0530

    Set the pgAdmin executable file permissions to 755 for the web rpm build.

--- pkg/redhat/build.sh
+++ pkg/redhat/build.sh
@@ -124,10 +124,7 @@ cp -rfa %{pga_build_root}/desktop/* \${RPM_BUILD_ROOT}
 
 %files
 %defattr(-,root,root,755)
-%attr(755,root,root) /usr/pgadmin4/bin/setup-web.sh
 %attr(755,root,root) /usr/pgadmin4/bin/pgadmin4
-%attr(755,root,root) /usr/pgadmin4/web/pgacloud/pgacloud.py
-%attr(755,root,root) /usr/pgadmin4/web/pgadmin/misc/bgprocess/process_executor.py
 /usr/pgadmin4/bin/*
 /usr/share/icons/hicolor/128x128/apps/*
 /usr/share/icons/hicolor/64x64/apps/*
@@ -179,6 +176,9 @@ cp -rfa %{pga_build_root}/web/* \${RPM_BUILD_ROOT}
 %files
 %defattr(-,root,root,755)
 /usr/pgadmin4/bin/*
+%attr(755,root,root) /usr/pgadmin4/bin/setup-web.sh
+%attr(755,root,root) /usr/pgadmin4/web/pgacloud/pgacloud.py
+%attr(755,root,root) /usr/pgadmin4/web/pgadmin/misc/bgprocess/process_executor.py
 %config(noreplace) /etc/httpd/conf.d/*
 /usr/pgadmin4/sbom-web.json
 EOF
