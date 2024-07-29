636c224bd0132c4d162d6e8d51a9af9dc693f550
Akshay Joshi <akshay.joshi@enterprisedb.com>
2024-07-29 19:34:43 +0530

    Updated JavaScript and Python packages according to Dependabot recommendations

--- pkg/mac/build-functions.sh
+++ pkg/mac/build-functions.sh
@@ -135,7 +135,7 @@ _build_docs() {
     source "${BUILD_ROOT}/venv/bin/activate"
     pip3 install --upgrade pip
     pip3 install -r "${SOURCE_DIR}/requirements.txt"
-    pip3 install sphinx==6.1.3
+    pip3 install sphinx==7.4.7
     pip3 install sphinxcontrib-youtube
 
     cd "${SOURCE_DIR}" || exit
