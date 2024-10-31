bb4777578aff216c2c7a7c6b7844532d8fb91c15
Aditya Toshniwal <aditya.toshniwal@enterprisedb.com>
2024-10-31 18:07:26 +0530

    Remove bootstrap from the runtime code and update both yarn.lock files. #8077

--- runtime/src/js/menu.js
+++ runtime/src/js/menu.js
@@ -44,15 +44,15 @@ function buildMenu(pgadminMenus, pgAdminMainScreen, callbacks) {
 
   if (isMac) {
   // Remove About pgAdmin 4 from help menu and add it to the top of menuFile submenu.
-  const helpMenu = pgadminMenus.find((menu) => menu.name == 'help');
-  if (helpMenu) {
-    const aboutItem = helpMenu.submenu.find((item) => item.name === 'mnu_about');
-    if (aboutItem) {
-      helpMenu.submenu = helpMenu.submenu.filter((item) => item.name !== 'mnu_about');
-      menuFile.submenu.unshift(aboutItem);
-      menuFile.submenu.splice(1, 0, { type: 'separator' });
+    const helpMenu = pgadminMenus.find((menu) => menu.name == 'help');
+    if (helpMenu) {
+      const aboutItem = helpMenu.submenu.find((item) => item.name === 'mnu_about');
+      if (aboutItem) {
+        helpMenu.submenu = helpMenu.submenu.filter((item) => item.name !== 'mnu_about');
+        menuFile.submenu.unshift(aboutItem);
+        menuFile.submenu.splice(1, 0, { type: 'separator' });
+      }
     }
-  }
   }
   
   template.push({
