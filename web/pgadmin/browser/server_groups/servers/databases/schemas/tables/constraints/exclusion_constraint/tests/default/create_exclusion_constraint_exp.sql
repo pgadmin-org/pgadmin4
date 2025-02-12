200954f58c429aa058fc533fd103cbdcb1f96792
pravesh-sharma <pravesh.sharma@enterprisedb.com>
2025-02-12 17:25:07 +0530

    Fixed an issue where the column order displayed was incorrect for exclusion constraints with multiple columns. #8430

--- web/pgadmin/browser/server_groups/servers/databases/schemas/tables/constraints/exclusion_constraint/tests/default/create_exclusion_constraint_exp.sql
+++ web/pgadmin/browser/server_groups/servers/databases/schemas/tables/constraints/exclusion_constraint/tests/default/create_exclusion_constraint_exp.sql
@@ -4,8 +4,8 @@
 
 ALTER TABLE IF EXISTS testschema.tableforexclusion
     ADD CONSTRAINT "Exclusion_$%{}[]()&*^!@""'`\/#" EXCLUDE USING gist (
-    (col1 + col3) WITH <>,
-    col2 WITH <>)
+    col2 WITH <>,
+    (col1 + col3) WITH <>)
     WITH (FILLFACTOR=12)
     WHERE (col1 > 1)
     DEFERRABLE INITIALLY DEFERRED;
