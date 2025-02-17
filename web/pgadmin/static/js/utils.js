fd63c63674f86fa343bb1365d32b393ac4cdee5d
Anil Sahoo <anil.sahoo@enterprisedb.com>
2025-02-17 12:13:12 +0530

    Fixed an issue where editing pasted JSON data type values in the Result Grid caused a syntax error by replacing custom CSV-to-array conversion code with the PapaParse package. #8296

--- web/pgadmin/static/js/utils.js
+++ web/pgadmin/static/js/utils.js
@@ -11,6 +11,7 @@ import _ from 'lodash';
 import gettext from 'sources/gettext';
 import { hasTrojanSource } from 'anti-trojan-source';
 import convert from 'convert-units';
+import Papa from 'papaparse';
 import getApiInstance from './api_instance';
 import usePreferences from '../../preferences/static/js/store';
 import pgAdmin from 'sources/pgadmin';
@@ -261,74 +262,17 @@ export function sprintf(i_str) {
   }
 }
 
-// Modified ref: http://stackoverflow.com/a/1293163/2343 to suite pgAdmin.
 // This will parse a delimited string into an array of arrays.
 export function CSVToArray(strData, strDelimiter, quoteChar){
-  strDelimiter = strDelimiter || ',';
-  quoteChar = quoteChar || '"';
 
-  // Create a regular expression to parse the CSV values.
-  let objPattern = new RegExp(
-    (
-    // Delimiters.
-      '(\\' + strDelimiter + '|\\r?\\n|\\r|^)' +
-            // Quoted fields.
-            (quoteChar == '"' ? '(?:"([^"]*(?:""[^"]*)*)"|' : '(?:\'([^\']*(?:\'\'[^\']*)*)\'|') +
-            // Standard fields.
-            (quoteChar == '"' ? '([^"\\' + strDelimiter + '\\r\\n]*))': '([^\'\\' + strDelimiter + '\\r\\n]*))')
-    ),
-    'gi'
-  );
+  // Use papaparse to parse the CSV data
+  const parsedResult = Papa.parse(strData, {
+    delimiter: strDelimiter,
+    quoteChar: quoteChar,
+  });
 
-  // Create an array to hold our data. Give the array
-  // a default empty first row.
-  let arrData = [[]];
-
-  // The regex doesn't handle and skips start value if
-  // string starts with delimiter
-  if(strData.startsWith(strDelimiter)) {
-    arrData[ arrData.length - 1 ].push(null);
-  }
-
-  // Create an array to hold our individual pattern
-  // matching groups.
-  let arrMatches = null;
-
-  // Keep looping over the regular expression matches
-  // until we can no longer find a match.
-  while ((arrMatches = objPattern.exec( strData ))){
-    // Get the delimiter that was found.
-    let strMatchedDelimiter = arrMatches[ 1 ];
-
-    // Check to see if the given delimiter has a length
-    // (is not the start of string) and if it matches
-    // field delimiter. If id does not, then we know
-    // that this delimiter is a row delimiter.
-    if (strMatchedDelimiter.length && strMatchedDelimiter !== strDelimiter){
-      // Since we have reached a new row of data,
-      // add an empty row to our data array.
-      arrData.push( [] );
-    }
-
-    let strMatchedValue;
-
-    // Now that we have our delimiter out of the way,
-    // let's check to see which kind of value we
-    // captured (quoted or unquoted).
-    if (arrMatches[ 2 ]){
-      // We found a quoted value. When we capture
-      // this value, unescape any quotes.
-      strMatchedValue = arrMatches[ 2 ].replace(new RegExp( quoteChar+quoteChar, 'g' ), quoteChar);
-    } else {
-      // We found a non-quoted value.
-      strMatchedValue = arrMatches[ 3 ];
-    }
-    // Now that we have our value string, let's add
-    // it to the data array.
-    arrData[ arrData.length - 1 ].push( strMatchedValue );
-  }
   // Return the parsed data.
-  return arrData;
+  return parsedResult.data;
 }
 
 export function hasBinariesConfiguration(pgBrowser, serverInformation) {
