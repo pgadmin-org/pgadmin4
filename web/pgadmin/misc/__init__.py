3037f25eb1189f342bcc404625eaac651f06e591
Anil Sahoo <anil.sahoo@enterprisedb.com>
2025-03-24 11:45:51 +0530

    Fixed an issue where the upgrade_check API returned an unexpected keyword argument 'cafile' due to changes in the urllib package supporting Python v3.13. #8577

--- web/pgadmin/misc/__init__.py
+++ web/pgadmin/misc/__init__.py
@@ -29,6 +29,8 @@ import threading
 import time
 import json
 import os
+import sys
+import ssl
 from urllib.request import urlopen
 from pgadmin.settings import get_setting, store_setting
 
@@ -324,6 +326,16 @@ def validate_binary_path():
     return make_json_response(data=gettext(version_str), status=200)
 
 
+def urlopen_with_ssl(url, data, timeout, cafile):
+    context = ssl.create_default_context(cafile=cafile)
+    if sys.version_info >= (3, 13):
+        # Use SSL context for Python 3.13+
+        return urlopen(url, data=data, timeout=timeout, context=context)
+    else:
+        # Use cafile parameter for older versions
+        return urlopen(url, data=data, timeout=timeout, cafile=cafile)
+
+
 @blueprint.route("/upgrade_check", endpoint="upgrade_check",
                  methods=['GET'])
 @pga_login_required
@@ -346,7 +358,8 @@ def upgrade_check():
                 # It stuck on rendering the browser.html, while working in the
                 # broken network.
                 if os.path.exists(config.CA_FILE):
-                    response = urlopen(url, data, 5, cafile=config.CA_FILE)
+                    response = urlopen_with_ssl(
+                        url, data, 5, config.CA_FILE)
                 else:
                     response = urlopen(url, data, 5)
                 current_app.logger.debug(
