8253e9965ee325f65a5fb596f9856b7303b4eddb
Allen Hernandez <2349718+AllenSH12@users.noreply.github.com>
2025-05-21 13:11:16 -0400

    does it with only gunicorn_config.py

--- pkg/docker/entrypoint.sh
+++ pkg/docker/entrypoint.sh
@@ -114,6 +114,7 @@ if [ ! -f /var/lib/pgadmin/pgadmin4.db ] && [ "${external_config_db_exists}" = "
     fi
      email_config="{'CHECK_EMAIL_DELIVERABILITY': ${CHECK_EMAIL_DELIVERABILITY}, 'ALLOW_SPECIAL_EMAIL_DOMAINS': ${ALLOW_SPECIAL_EMAIL_DOMAINS}, 'GLOBALLY_DELIVERABLE': ${GLOBALLY_DELIVERABLE}}"
      # issues/8665
+     echo 'JSON LOGGING' $PGADMIN_CONFIG_JSON_LOGGER
      echo "email config is ${email_config}"
      is_valid_email=$(cd /pgadmin4/pgadmin/utils && /venv/bin/python3 -c "from validation_utils import validate_email; val = validate_email('${PGADMIN_DEFAULT_EMAIL}', ${email_config}); print(val)")
      if echo "${is_valid_email}" | grep "False" > /dev/null; then
@@ -165,6 +166,7 @@ elif [ "${PGADMIN_REPLACE_SERVERS_ON_STARTUP}" = "True" ]; then
 fi
 
 # Start Postfix to handle password resets etc.
+# issues/8665
 if [ -z "${PGADMIN_DISABLE_POSTFIX}" ]; then
     sudo /usr/sbin/postfix start
 fi
@@ -189,5 +191,5 @@ fi
 if [ -n "${PGADMIN_ENABLE_TLS}" ]; then
     exec /venv/bin/gunicorn --limit-request-line "${GUNICORN_LIMIT_REQUEST_LINE:-8190}" --timeout "${TIMEOUT}" --bind "${BIND_ADDRESS}" -w 1 --threads "${GUNICORN_THREADS:-25}" --access-logfile "${GUNICORN_ACCESS_LOGFILE:--}" --keyfile /certs/server.key --certfile /certs/server.cert -c gunicorn_config.py run_pgadmin:app
 else
-    exec /venv/bin/gunicorn --log-config gunicorn_logging.conf --limit-request-line "${GUNICORN_LIMIT_REQUEST_LINE:-8190}" --timeout "${TIMEOUT}" --bind "${BIND_ADDRESS}" -w 1 --threads "${GUNICORN_THREADS:-25}" --access-logfile "${GUNICORN_ACCESS_LOGFILE:--}" -c gunicorn_config.py run_pgadmin:app
+    exec /venv/bin/gunicorn --limit-request-line "${GUNICORN_LIMIT_REQUEST_LINE:-8190}" --timeout "${TIMEOUT}" --bind "${BIND_ADDRESS}" -w 1 --threads "${GUNICORN_THREADS:-25}" --access-logfile "${GUNICORN_ACCESS_LOGFILE:--}" -c gunicorn_config.py run_pgadmin:app
 fi
--- pkg/docker/gunicorn_config.py
+++ pkg/docker/gunicorn_config.py
@@ -1,2 +1,33 @@
 import gunicorn
-gunicorn.SERVER_SOFTWARE = 'Python'
+import logging
+
+gunicorn.SERVER_SOFTWARE = "Python"
+
+
+def on_starting(worker):
+    # Can be resolved because of how Dockerfile organizes the files
+    import config
+
+    if not config.JSON_LOGGER:
+        return
+
+    from jsonformatter import JsonFormatter
+
+    json_formatter = JsonFormatter(config.CONSOLE_LOG_FORMAT_JSON)
+
+    # Apply JSON formatter to root logs
+    root_logger = logging.getLogger("root")
+    for handler in root_logger.handlers:
+        handler.setFormatter(json_formatter)
+
+    # Apply JSON formatter to access logs
+    access_logger = logging.getLogger("gunicorn.access")
+    access_logger.setLevel(config.CONSOLE_LOG_LEVEL)
+    for handler in access_logger.handlers:
+        handler.setFormatter(json_formatter)
+
+    # Apply JSON formatter to error logs
+    error_logger = logging.getLogger("gunicorn.error")
+    error_logger.setLevel(config.CONSOLE_LOG_LEVEL)
+    for handler in error_logger.handlers:
+        handler.setFormatter(json_formatter)
