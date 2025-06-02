4a4d4569ae4f99274bdd65b59e964fb59944a6b3
Aditya Toshniwal <aditya.toshniwal@enterprisedb.com>
2025-06-02 15:58:07 +0530

    Improved PL/PGSQL code folding and support nested blocks. #6118

--- docs/en_US/release_notes_9_5.rst
+++ docs/en_US/release_notes_9_5.rst
@@ -28,4 +28,6 @@ Housekeeping
 Bug fixes
 *********
 
+  | `Issue #6118 <https://github.com/pgadmin-org/pgadmin4/issues/6118>`_ -  Improved PL/pgSQL code folding and support nested blocks.
   | `Issue #8032 <https://github.com/pgadmin-org/pgadmin4/issues/8032>`_ -  Fixed an issue where the Schema Diff Tool incorrectly reported differences due to variations in the order of the privileges.
+  | `Issue #8691 <https://github.com/pgadmin-org/pgadmin4/issues/8691>`_ -  Fixed an issue in the query tool where using multiple cursors to copy text resulted in only the first line being copied.
--- web/pgadmin/static/js/components/ReactCodeMirror/extensions/dialect.js
+++ web/pgadmin/static/js/components/ReactCodeMirror/extensions/dialect.js
@@ -1,4 +1,5 @@
 import { SQLDialect, PostgreSQL } from '@codemirror/lang-sql';
+import { foldNodeProp } from '@codemirror/language';
 
 const extraKeywords = 'unsafe';
 const keywords = PostgreSQL.spec.keywords.replace(/\b\w\b/, '') + ' ' + extraKeywords;
@@ -10,5 +11,14 @@ const PgSQL = SQLDialect.define({
   specialVar: '',
   keywords: keywords,
   types: PostgreSQL.spec.types,
+}).configureLanguage({
+  // Disable default folding behavior as it conflicts with custom folding
+  props: [
+    foldNodeProp.add({
+      Statement() { return null; },
+      BlockComment() { return null; }
+    }),
+  ]
 });
+
 export default PgSQL;
