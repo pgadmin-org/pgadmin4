181d30728550d04d696ab8ef4d170b274adc1a27
Murtuza Zabuawala <murtuza.zabuawala@enterprisedb.com>
2025-07-19 13:43:30 +0530

    Fixes sql tests #8969

--- web/pgadmin/browser/server_groups/servers/databases/schemas/tables/indexes/tests/default/create_btree_asc_null_first_msql.sql
+++ web/pgadmin/browser/server_groups/servers/databases/schemas/tables/indexes/tests/default/create_btree_asc_null_first_msql.sql
@@ -1,7 +1,7 @@
 CREATE UNIQUE INDEX "Idx_$%{}[]()&*^!@""'`\/#"
     ON public.test_table_for_indexes USING btree
     (id ASC NULLS FIRST, name COLLATE pg_catalog."POSIX" text_pattern_ops ASC NULLS FIRST)
-    WITH (fillfactor=10)
+    WITH (fillfactor=10, deduplicate_items=True)
     TABLESPACE pg_default
     WHERE id < 100;
 
