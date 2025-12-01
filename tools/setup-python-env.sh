dc7873c2b25742ab22294053048d3d3e609df9cf
Akshay Joshi <akshay.joshi@enterprisedb.com>
2025-12-01 19:13:04 +0530

    Update Python and JavaScript dependecies.

--- tools/setup-python-env.sh
+++ tools/setup-python-env.sh
@@ -18,8 +18,8 @@ pip install --upgrade pip
 if [ ${TEST} -eq 1 ];
 then
   echo Installing requirements for running Python tests...
-  pip install --force-reinstall --no-cache-dir wheel sphinx==7.4.7 sphinxcontrib-youtube -r web/regression/requirements.txt || { echo 'ERROR: Failed to install Python requirements.' ; exit 1; }
+  pip install --force-reinstall --no-cache-dir wheel sphinx==9.0.0 sphinxcontrib-youtube -r web/regression/requirements.txt || { echo 'ERROR: Failed to install Python requirements.' ; exit 1; }
 else
   echo Installing requirements for executing and building only...
-  pip install --force-reinstall --no-cache-dir wheel sphinx==7.4.7 sphinxcontrib-youtube -r requirements.txt || { echo 'ERROR: Failed to install Python requirements.' ; exit 1; }
+  pip install --force-reinstall --no-cache-dir wheel sphinx==9.0.0 sphinxcontrib-youtube -r requirements.txt || { echo 'ERROR: Failed to install Python requirements.' ; exit 1; }
 fi
