bc1825eb29387c02314aeb4540c35e47ea69f620
Murtuza Zabuawala <murtuza.zabuawala@enterprisedb.com>
2026-02-03 15:52:12 +0530

    Feat: Added a Statistics node

---
+++ web/pgadmin/browser/server_groups/servers/databases/schemas/statistics/templates/statistics/sql/16_plus/update.sql
@@ -0,0 +1,31 @@
+{### SQL to update extended statistics object (PostgreSQL 16+) ###}
+{### Rename statistics ###}
+{% if data.name and data.name != o_data.name %}
+ALTER STATISTICS {% if data.schema and data.schema != o_data.schema %}{{ conn|qtIdent(o_data.schema, o_data.name) }}{% else %}{{ conn|qtIdent(o_data.schema, o_data.name) }}{% endif %}
+
+    RENAME TO {{ conn|qtIdent(data.name) }};
+
+{% endif %}
+{### Change schema ###}
+{% if data.schema and data.schema != o_data.schema %}
+ALTER STATISTICS {{ conn|qtIdent(o_data.schema, data.name if data.name else o_data.name) }}
+    SET SCHEMA {{ conn|qtIdent(data.schema) }};
+
+{% endif %}
+{### Change owner ###}
+{% if data.owner and data.owner != o_data.owner %}
+ALTER STATISTICS {{ conn|qtIdent(data.schema if data.schema else o_data.schema, data.name if data.name else o_data.name) }}
+    OWNER TO {{ conn|qtIdent(data.owner) }};
+
+{% endif %}
+{### Set statistics target (PostgreSQL 13+) ###}
+{% if data.stattarget is defined and data.stattarget != o_data.stattarget %}
+ALTER STATISTICS {{ conn|qtIdent(data.schema if data.schema else o_data.schema, data.name if data.name else o_data.name) }}
+    SET STATISTICS {{ data.stattarget }};
+
+{% endif %}
+{### Update comment ###}
+{% if data.comment is defined and data.comment != o_data.comment %}
+COMMENT ON STATISTICS {{ conn|qtIdent(data.schema if data.schema else o_data.schema, data.name if data.name else o_data.name) }}
+    IS {% if data.comment %}{{ data.comment|qtLiteral(conn) }}{% else %}NULL{% endif %};
+{% endif %}
