b7b02c9a5b032cf0676a3d00896836576dff07a5
Rohit Bhati <rohit.bhati@enterprisedb.com>
2026-03-17 18:46:34 +0530

    Fixed View/Edit Data not handling generated columns properly. #9672

--- web/pgadmin/tools/sqleditor/templates/sqleditor/sql/default/update.sql
+++ web/pgadmin/tools/sqleditor/templates/sqleditor/sql/default/update.sql
@@ -4,4 +4,7 @@ UPDATE {{ conn|qtIdent(nsp_name, object_name) | replace("%", "%%") }} SET
 {% if not loop.first %}, {% endif %}{{ conn|qtIdent(col) | replace("%", "%%") }} = %({{ pgadmin_alias[col] }})s{% if type_cast_required[col] %}::{{ data_type[col] }}{% endif %}{% endfor %}
  WHERE
 {% for pk in primary_keys %}
-{% if not loop.first %} AND {% endif %}{{ conn|qtIdent(pk) | replace("%", "%%") }} = {{ primary_keys[pk]|qtLiteral(conn) }}{% endfor %};
+{% if not loop.first %} AND {% endif %}{{ conn|qtIdent(pk) | replace("%", "%%") }} = {{ primary_keys[pk]|qtLiteral(conn) }}{% endfor %}
+{# Return primary keys to refetch row with recalculated generated column values #}
+{% if pk_names and not has_oids %} RETURNING {{pk_names | replace("%", "%%")}}{% endif %}
+{% if has_oids %} RETURNING oid{% endif %};
