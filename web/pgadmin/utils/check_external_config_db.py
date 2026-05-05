42a287bc26269a30fb847471fa0edff4b20906f4
Yogesh Mahajan <yogesh.mahajan@enterprisedb.com>
2026-05-05 10:44:28 +0530

    Ensure to use psycopg3 is used while connecting to postgres DB when PGADMIN_CONFIG_CONFIG_DATABASE_URI is specified.

--- web/pgadmin/utils/check_external_config_db.py
+++ web/pgadmin/utils/check_external_config_db.py
@@ -15,6 +15,9 @@ def check_external_config_db(database_uri):
     Check if external config database exists if it
     is being used.
     """
+    if database_uri.startswith("postgresql://") or database_uri.startswith(
+            "postgres://"):
+        database_uri = database_uri.replace("://", "+psycopg://", 1)
     engine = create_engine(database_uri)
     try:
         connection = engine.connect()
