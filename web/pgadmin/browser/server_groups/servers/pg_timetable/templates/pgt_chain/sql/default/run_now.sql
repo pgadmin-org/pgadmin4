edbae34c4373d934a6819e769276070491c4fcb2
Regina Obe <lr@pcorp.us>
2026-07-16 16:12:40 -0400

    Take care of issues raised during code review
     - run_now
    	- when no client_name specified in the chain, run on all active agents
    	- if no active agent and no client_name specified in chain, raise an error
     - fix missing :
     - fix parsing / deparsing of json data in timetable.parameter

--- web/pgadmin/browser/server_groups/servers/pg_timetable/templates/pgt_chain/sql/default/run_now.sql
+++ web/pgadmin/browser/server_groups/servers/pg_timetable/templates/pgt_chain/sql/default/run_now.sql
@@ -1,8 +1,26 @@
-SELECT timetable.notify_chain_start(
-    {{ chain_id|qtLiteral(conn) }}::bigint,
-    (
-        SELECT client_name
-        FROM timetable.chain
-        WHERE chain_id = {{ chain_id|qtLiteral(conn) }}::integer
-    )
-) AS result;
+DO $$
+DECLARE
+    _client_name text;
+    _chain_id bigint := {{ chain_id|qtLiteral(conn) }}::bigint;
+    _found boolean := false;
+BEGIN
+    SELECT client_name INTO _client_name
+    FROM timetable.chain
+    WHERE chain_id = _chain_id;
+
+    IF _client_name IS NULL OR _client_name = '' THEN
+        FOR _client_name IN
+            SELECT DISTINCT client_name FROM timetable.active_session
+        LOOP
+            _found := true;
+            PERFORM timetable.notify_chain_start(_chain_id, _client_name);
+        END LOOP;
+
+        IF NOT _found THEN
+            RAISE EXCEPTION 'no client_name specified and no active sessions';
+        END IF;
+    ELSE
+        PERFORM timetable.notify_chain_start(_chain_id, _client_name);
+    END IF;
+END
+$$;
